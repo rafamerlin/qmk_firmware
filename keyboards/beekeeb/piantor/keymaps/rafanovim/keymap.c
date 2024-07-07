87ca48eced5df7a25fc634a83d8453447c6fcb3b
Rafael Merlin <rafamerlin@gmail.com>
2024-07-07 14:24:04 +1000

    Add gaming layer

--- keyboards/beekeeb/piantor/keymaps/rafanovim/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/rafanovim/keymap.c
@@ -165,13 +165,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // gaming
     [7] = LAYOUT_split_3x5_3(
   //,--------------------------------------------.                    ,---------------------------------------------.
-         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+         KC_Q,    KC_1,    KC_2,    KC_3,    KC_4,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
   //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+|
-         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,
+      KC_LCTL,    KC_A,    KC_W,    KC_D,    KC_E,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,
   //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+|
-         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,
+      KC_LSFT,    KC_C,    KC_S,    KC_R,    KC_F,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,
   //|--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+|
-                              XXXXXXX, _______,  KC_ENT,     KC_SPC, _______, XXXXXXX
+                              KC_X, KC_SPC,  KC_G,                     KC_SPC, _______, TO(0)
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -184,7 +184,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+|
       QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MPLY,
   //|--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+|
-                                 XXXXXXX, XXXXXXX,  KC_ENT,    XXXXXXX, KC_APP, KC_APP 
+                                   TO(7), XXXXXXX,  KC_ENT,    XXXXXXX, KC_APP, KC_APP 
                                       //`--------------------------'  `--------------------------'
   ),
 
